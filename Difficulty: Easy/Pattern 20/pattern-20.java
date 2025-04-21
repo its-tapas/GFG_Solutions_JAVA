f57c408bfa45212eb1177cd7443e574d5269e287
Tapas Singhal <98687345+its-tapas@users.noreply.github.com>
2025-04-22 03:25:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern 20/pattern-20.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int N) {
+    int iniS = 2*N -2;
+      for(int i=1;i<N;i++){
+          
+          for(int j=1;j<=i;j++){
+              System.out.print("*");
+          }
+          
+          for(int j=0;j<iniS;j++){
+              System.out.print(" ");
+          }
+          
+          for(int j=1;j<=i;j++){
+              System.out.print("*");
+          }
+          
+          iniS-=2;
+          
+          System.out.println();
+      }
+      
+    iniS = 0;
+    for(int i=0;i< N;i++){
+          
+
+          for(int j=1;j<=N-i;j++){
+              System.out.print("*");
+          }
+          
+          for(int j=0;j<iniS;j++){
+              System.out.print(" ");
+          }
+          
+          for(int j=1;j<=N-i;j++){
+              System.out.print("*");
+          }
+          
+          iniS+=2;
+          
+          System.out.println();
+      }
+      
+      
+      
+    }
+}
