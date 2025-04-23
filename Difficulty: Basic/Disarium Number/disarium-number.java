9c3d123172a8399545e3f204d3852779d4feaca9
Tapas Singhal <98687345+its-tapas@users.noreply.github.com>
2025-04-24 03:01:15 +0530

    Update disarium-number.java

--- Difficulty: Basic/Disarium Number/disarium-number.java
+++ Difficulty: Basic/Disarium Number/disarium-number.java
@@ -26,22 +26,33 @@ System.out.println("~");
 
 class Solution {
     static int isDisarium(int n) {
-        // code here
+        // Method - 1 : Reversing the digit and set a position tracker
         int i=1,N=0,a=n;
         double sum=0;
         while(a>0){
             N=N*10+a%10;
             a=a/10;
         }
+        
         int p=0;
         while(N>0){
             p=N%10;
             sum=sum+Math.pow(p,i);
-            
             i++;
             N/=10;
         }
         if (sum==n) return 1;
         else return 0;
+
+        /* Method - 2: String Conversion
+        String str = Integer.toString(n);
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int digit = Character.getNumericValue(str.charAt(i));
+            sum += Math.pow(digit, i + 1);  // position starts from 1
+        }
+        return sum == n ? 1 : 0;
+        */
+            
     }
 };
