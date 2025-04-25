a85b93c4fb4347acd628b0f308507cc0348c25a2
Tapas Singhal <98687345+its-tapas@users.noreply.github.com>
2025-04-26 00:51:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Prime Number/prime-number.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            if (ob.isPrime(n)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        int c=0;
+        int N=(int)Math.sqrt(n);
+        for (int i=2;i<=N;i++){
+            if(n%i==0) c++;
+        }
+        return c==0;
+    }
+}
